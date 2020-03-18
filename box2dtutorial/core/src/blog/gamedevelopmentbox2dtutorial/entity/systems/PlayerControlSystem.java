b796ab7756dbd99eeaf0865a011dad009ca12b48
emilstu <emilstubsjoen@gmail.com>
2020-03-18 01:01:13 +0100

    .

--- box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/systems/PlayerControlSystem.java
+++ box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/systems/PlayerControlSystem.java
@@ -82,16 +82,15 @@ public class PlayerControlSystem extends IteratingSystem{
          */
 
         if(player.onSpring){
-            b2body.body.applyForceToCenter(0, 50, true);
-            b2body.body.applyLinearImpulse(0, 50f, b2body.body.getWorldCenter().x, b2body.body.getWorldCenter().y, true);
+            //b2body.body.applyForceToCenter(0, 50, true);
+            b2body.body.applyLinearImpulse(0, 70f, b2body.body.getWorldCenter().x, b2body.body.getWorldCenter().y, true);
             state.set(StateComponent.STATE_JUMPING);
             player.onSpring = false;
         }
 
         if (controller.space && player.superSpeed){
             //b2body.body.setLinearVelocity(MathUtils.lerp(b2body.body.getLinearVelocity().x, 5000, 0.01f),b2body.body.getLinearVelocity().y);
-            b2body.body.applyLinearImpulse(10000f, 0f, b2body.body.getWorldCenter().x, b2body.body.getWorldCenter().y, true);
-
+            b2body.body.applyLinearImpulse(50000f, 0f, b2body.body.getWorldCenter().x, b2body.body.getWorldCenter().y, true);
             player.superSpeed = false;
 
 
