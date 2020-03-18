a5e061e55351be1ee0454cbfca413bb17bcdde26
emilstu <emilstubsjoen@gmail.com>
2020-03-18 14:03:32 +0100

    Updated to store a new variable shootable

--- box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/components/TypeComponent.java
+++ box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/components/TypeComponent.java
@@ -1,8 +1,9 @@
 package blog.gamedevelopmentbox2dtutorial.entity.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
 
-public class TypeComponent implements Component {
+public class TypeComponent implements Component, Pool.Poolable {
     public static final int PLAYER = 0;
     public static final int ENEMY = 1;
     public static final int SCENERY = 2;
@@ -10,8 +11,17 @@ public class TypeComponent implements Component {
     public static final int GROUND = 4;
     public static final int GUN = 5;
     public static final int SPRING = 6;
-
-    public static final int OTHER = 7;
+    public static  final int BULLET = 7;
+    public static final int OTHER = 8;
 
     public int type = OTHER;
+    public boolean shootable = false;
+
+    @Override
+    public void reset() {
+        type = OTHER;
+        shootable = false;
+    }
+
+
 }
