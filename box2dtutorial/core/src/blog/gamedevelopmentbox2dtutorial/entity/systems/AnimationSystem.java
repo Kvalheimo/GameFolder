9cb07d4f1f6d3fdd82053427200172398b6b6bab
emilstu <emilstubsjoen@gmail.com>
2020-03-19 11:07:30 +0100

    Endret så animation skal fungere

--- box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/systems/AnimationSystem.java
+++ box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/systems/AnimationSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import blog.gamedevelopmentbox2dtutorial.entity.components.AnimationComponent;
 import blog.gamedevelopmentbox2dtutorial.entity.components.StateComponent;
@@ -15,30 +16,28 @@ public class AnimationSystem extends IteratingSystem {
     ComponentMapper<AnimationComponent> am;
     ComponentMapper<StateComponent> sm;
 
-
     @SuppressWarnings("unchecked")
     public AnimationSystem(){
-        super(Family.all(TextureComponent.class, AnimationComponent.class, StateComponent.class).get());
+        super(Family.all(TextureComponent.class,
+                AnimationComponent.class,
+                StateComponent.class).get());
 
         tm = ComponentMapper.getFor(TextureComponent.class);
         am = ComponentMapper.getFor(AnimationComponent.class);
         sm = ComponentMapper.getFor(StateComponent.class);
     }
 
-
     @Override
-    protected void processEntity(Entity entity, float dt) {
-        AnimationComponent ani =  am.get(entity);
+    protected void processEntity(Entity entity, float deltaTime) {
+
+        AnimationComponent ani = am.get(entity);
         StateComponent state = sm.get(entity);
 
         if(ani.animations.containsKey(state.get())){
             TextureComponent tex = tm.get(entity);
-            //tex.region = ani.animations.get(state.get()).getKeyFrame(state.time, state.isLooping);
+            tex.region = (TextureRegion) ani.animations.get(state.get()).getKeyFrame(state.time, state.isLooping);
         }
-        state.time += dt;
 
-
+        state.time += deltaTime;
     }
-
 }
-
