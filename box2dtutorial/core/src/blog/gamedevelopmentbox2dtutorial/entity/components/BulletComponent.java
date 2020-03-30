bf6bf9452136cdb3ba06710be9173eaf79ff0765
emilstu <emilstubsjoen@gmail.com>
2020-03-30 16:47:12 +0200

    La til particle effect component (slik at det kan kobles til en body)

--- box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/components/BulletComponent.java
+++ box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/components/BulletComponent.java
@@ -1,9 +1,11 @@
 package blog.gamedevelopmentbox2dtutorial.entity.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.utils.Pool;
 
 public class BulletComponent implements Component, Pool.Poolable {
+    public Entity particleEffect;
     public float xVel;
     public float yVel;
     public boolean isDead = false;
@@ -14,6 +16,7 @@ public class BulletComponent implements Component, Pool.Poolable {
         xVel = 0;
         yVel = 0;
         isDead = false;
+        particleEffect = null;
 
     }
 
