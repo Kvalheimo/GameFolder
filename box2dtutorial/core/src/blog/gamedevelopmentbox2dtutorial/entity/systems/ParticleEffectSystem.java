ae196e3751837fb63e1ec4b96773b38ef7160ab1
emilstu <emilstubsjoen@gmail.com>
2020-03-30 16:51:28 +0200

    Particle effect system

---
+++ box2dtutorial/core/src/blog/gamedevelopmentbox2dtutorial/entity/systems/ParticleEffectSystem.java
@@ -0,0 +1,68 @@
+package blog.gamedevelopmentbox2dtutorial.entity.systems;
+
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+
+import blog.gamedevelopmentbox2dtutorial.entity.components.Mapper;
+import blog.gamedevelopmentbox2dtutorial.entity.components.ParticleEffectComponent;
+
+public class ParticleEffectSystem extends IteratingSystem{
+
+    private static final boolean shouldRender = true;
+
+    private Array<Entity> renderQueue;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+
+
+    @SuppressWarnings("unchecked")
+    public ParticleEffectSystem(SpriteBatch sb, OrthographicCamera cam) {
+        super(Family.all(ParticleEffectComponent.class).get());
+        renderQueue = new Array<Entity>();
+        batch = sb;
+        camera = cam;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        batch.setProjectionMatrix(camera.combined);
+        batch.enableBlending();
+        // Render PE
+        if(shouldRender){
+            batch.begin();
+            for (Entity entity : renderQueue) {
+                ParticleEffectComponent paCom = Mapper.paCom.get(entity);
+                paCom.particleEffect.draw(batch, deltaTime);
+            }
+            batch.end();
+        }
+        renderQueue.clear();
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        ParticleEffectComponent paCom = Mapper.paCom.get(entity);
+        if(paCom.isDead){
+            paCom.timeTilDeath -= deltaTime;
+        }
+
+        // Move PE if attached
+        if(paCom.isAttached){
+            paCom.particleEffect.setPosition(
+                    paCom.attachedBody.getPosition().x + paCom.xOffset,
+                    paCom.attachedBody.getPosition().y + paCom.yOffset);
+        }
+        // free PE if completed
+        if(paCom.particleEffect.isComplete() || paCom.timeTilDeath <= 0){
+            getEngine().removeEntity(entity);
+        }else{
+            renderQueue.add(entity);
+        }
+    }
+}
