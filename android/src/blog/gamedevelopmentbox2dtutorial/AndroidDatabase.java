36336ef69a4f63430664596f81a8e6fd4666e955
petter <39340152+petterhs@users.noreply.github.com>
2020-04-14 19:16:43 +0200

    create opponens from database

--- android/src/blog/gamedevelopmentbox2dtutorial/AndroidDatabase.java
+++ android/src/blog/gamedevelopmentbox2dtutorial/AndroidDatabase.java
@@ -1,13 +1,22 @@
 package blog.gamedevelopmentbox2dtutorial;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.badlogic.gdx.math.Vector3;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.badlogic.ashley.core.Entity;
 
 
-import java.util.List;
+import java.util.ArrayList;
 
+import blog.gamedevelopmentbox2dtutorial.Factory.LevelFactory;
 import blog.gamedevelopmentbox2dtutorial.entity.components.PlayerComponent;
+import blog.gamedevelopmentbox2dtutorial.entity.components.TransformComponent;
 
 public class AndroidDatabase implements DatabaseHandler.DataBase {
 
@@ -18,14 +27,54 @@ public class AndroidDatabase implements DatabaseHandler.DataBase {
         db = FirebaseDatabase.getInstance();
     }
 
-    @Override
-    public void publishPlayer(Entity player) {
-        dbRef = db.getReference("message");
-        dbRef.setValue("Hello, World!");
+
+    public void addPlayerEventListener(final ArrayList<Entity> opponents, final LevelFactory levelFactory) {
+        dbRef = db.getReference("players/");
+        dbRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Iterable<DataSnapshot> players = dataSnapshot.getChildren();
+                for(DataSnapshot snapshot : players) {
+                    if (snapshot.child("x").exists()){
+                        Float posX = snapshot.child("x").getValue(Float.class);
+                        Vector3 pos = new Vector3(snapshot.child("x").getValue(Float.class), snapshot.child("y").getValue(Float.class),0);
+                        opponents.add(levelFactory.createOpponent(pos, 1));
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
-    public List<PlayerComponent> getPlayers() {
+    public void publishPlayer(String uniqueID, Entity player) {
+        dbRef = db.getReference("players/" + uniqueID + "/pos");
+        dbRef.child("x").setValue(player.getComponent(TransformComponent.class).position.x);
+        dbRef.child("y").setValue(player.getComponent(TransformComponent.class).position.y);
+    }
+
+    @Override
+    public ArrayList<Entity> getPlayers() {
+        dbRef = db.getReference("players/");
         return null;
     }
 }
