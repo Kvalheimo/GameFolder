119ea11d00bd894ca7b258a41bb3f9f9c1a4c55c
henrirov <henrikroven@gmail.com>
2020-04-16 09:38:53 +0200

    deleted unnecessary files

--- android/assets/maps/bakkegrafikk.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="bakkegrafikk" tilewidth="16" tileheight="16" tilecount="25" columns="5">
- <image source="../../../../../OneDrive/Documents/spill_grafikk/tiled_map/bakkegrafikk.png" width="84" height="84"/>
-</tileset>
--- android/assets/maps/tile_ground_3x3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="tile_ground_3x3" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="../../../../../OneDrive/Documents/spill_grafikk/tiled_map/tile_ground_3x3.png" width="48" height="48"/>
-</tileset>
