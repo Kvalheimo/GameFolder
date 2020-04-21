6a563370b2c54e74baa0259ac0f6a1bbfa2f39ed
emilstu <emilstubsjoen@gmail.com>
2020-04-22 01:43:45 +0200

    fixed merge coflict

--- core/src/blog/gamedevelopmentbox2dtutorial/entity/components/JumpWallComponent.java
+++ core/src/blog/gamedevelopmentbox2dtutorial/entity/components/JumpWallComponent.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool;
 
 public class JumpWallComponent implements Component, Pool.Poolable {
-    public float jumpWallPos = -0;
+    public float jumpWallPos = 0;
 
     @Override
     public void reset() {
-        jumpWallPos = -0;
+        jumpWallPos = 0;
     }
 
 }
