288c16c58db0811bbc587387879d20a5f0a6e361
emilstu <emilstubsjoen@gmail.com>
2020-04-26 18:00:38 +0200

    Fixed characterSelection

--- core/src/blog/gamedevelopmentbox2dtutorial/Box2dTutorial.java
+++ core/src/blog/gamedevelopmentbox2dtutorial/Box2dTutorial.java
@@ -93,7 +93,7 @@ public class Box2dTutorial extends Game {
 				levelSelectionScreen = new LevelSelectionScreen(this, character);
 				this.setScreen(levelSelectionScreen);
 				break;
-				case CHARACTER_SELECTION:
+			case CHARACTER_SELECTION:
 				if(characterSelectionScreen == null) characterSelectionScreen = new CharacterSelectionScreen(this);
 				this.setScreen(characterSelectionScreen);
 				break;
