e89b5989a3ecac63f2de72b53bfdf3c0529cf38e
petter <39340152+petterhs@users.noreply.github.com>
2020-04-26 13:48:28 +0200

    remove old resources and files

--- box2dtutorial/desktop/build/resources/main/maps/tileset/tileset_gutter.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2020.03.18" name="tileset_gutter" tilewidth="16" tileheight="16" tilecount="1147" columns="37">
- <image source="tileset_gutter.png" width="594" height="504"/>
-</tileset>
